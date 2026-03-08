d78991eb8ac45138cb78ced5597d7d132aaf1b4f
Arthur <arthurdev.contatoo@gmail.com>
2026-03-08 15:54:33 -0300

    modelando cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,35 @@
+namespace Model
+{
+    public class Cliente
+    {
+        #region Atributos 
+        public string Nome {get; set;}
+        public string SobreNome {get; set;}
+        public string NomeCompleto => $"{Nome} {SobreNome}";
+        public string Cpf {get; private set;}
+        public DateOnly DataNascimento {get; private set;}
+        public int Idade => CalcularIdade();
+        #endregion
+
+        public Cliente(string nome, string sobrenome, string cpf, DateOnly datanascimento)
+        {
+            Nome = nome;
+            SobreNome = sobrenome;
+            Cpf = cpf;
+            DataNascimento = datanascimento;
+        }
+
+        public int CalcularIdade()
+        {   
+            DateTime hoje = DateTime.Now;
+            int idade = hoje.Year - DataNascimento.Year;
+
+            if(hoje.Month < DataNascimento.Month || hoje.Month == DataNascimento.Month && hoje.Day < DataNascimento.Day)
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+    }
+}
